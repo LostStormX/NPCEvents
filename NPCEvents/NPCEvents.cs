99f68006bb37b5648bb263b81ee98b8e201c90d5
LostStormX <HunterBoyleston@gmail.com>
2024-11-21 23:25:26 -0500

    Add project files.

---
+++ NPCEvents/NPCEvents.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace Oxide.Plugins
+{
+    [Info("NPC Events", "DepressingDressing & LostStormX", "1.0.0")]
+    [Description("Creates and controls different NPC Events such as Bradley Crashes, ect.")]
+
+    class NPC_Events : RustPlugin
+    {
+        private float respawnDelay = 3600f;
+        private void SpawningBradleyAtLocation(Vector3 position)
+        {
+            BaseEntity bradley = GameManager.server.CreateEntity("assets/prefabs/npc/military/barricade/barricade.prefab", position, Quaternion.identity);
+
+            if (bradley != null)
+            {
+                bradley.Spawn();
+            }
+        }
+    }
+}
